bad9d661239d6d4bc84e19dda7ed53a5a0268770
5YEARS <tg.laroche@gmail.com>
2020-11-23 16:48:48 -0500

    Initial Commit

---
+++ Cutoff/Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player: MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 2.0f;
+    private float gravityValue = -9.81f;
+
+    public Transform cameraHolder;
+    public float mouseSensitivity = 2f;
+    public float upLimit = -50;
+    public float downLimit = 50;
+
+    private void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+        Rotate();
+    }
+
+
+    public void Rotate()
+    {
+        float horizontalRotation = Input.GetAxis("Mouse X");
+        float verticalRotation = Input.GetAxis("Mouse Y");
+
+        transform.Rotate(0, horizontalRotation * mouseSensitivity, 0);
+        cameraHolder.Rotate(-verticalRotation * mouseSensitivity, 0, 0);
+
+        Vector3 currentRotation = cameraHolder.localEulerAngles;
+        if (currentRotation.x > 180) currentRotation.x -= 360;
+        currentRotation.x = Mathf.Clamp(currentRotation.x, upLimit, downLimit);
+        cameraHolder.localRotation = Quaternion.Euler(currentRotation);
+    }
+}
+
